463f73657abfa00a1bc22021ea3a471f6e795e9c
Jorge Luis Ochoa <jorge.uchija2021@gmail.com>
2020-12-14 10:12:23 -0400

    reparacion de migraciones

--- db/migrate/20201209175849_change_column_in_transactions.rb
+++ db/migrate/20201209175849_change_column_in_transactions.rb
@@ -1,6 +1,9 @@
 class ChangeColumnInTransactions < ActiveRecord::Migration[6.0]
   def change
-    change_column :transactions, :monto_envio, :decimal, precision: 18, scale: 2
-    change_column :transactions, :monto_a_recibir, :decimal, precision: 18, scale: 2
+    remove_column :transactions, :monto_envio, :string
+    remove_column :transactions, :monto_a_recibir, :string
+
+    add_column :transactions, :monto_envio, :decimal, precision: 18, scale: 2
+    add_column :transactions, :monto_a_recibir, :decimal, precision: 18, scale: 2
   end
 end
