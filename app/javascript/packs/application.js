0dfab1ec2c531e6607f1b80ad82d000d1b6a0f99
Jorge Luis Ochoa <jorge.uchija2021@gmail.com>
2021-01-06 11:00:23 -0400

    insercion de logo preloader, boton de ir al cielo, y boton de volver atras

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,15 @@ import 'css/styles'
 
 import "@fortawesome/fontawesome-free/js/all";
 
+$(document).on('turbolinks:load', function() {
+
+    var contenedor = document.getElementById("loader");
+    contenedor.classList.add("efecto_desv")
+}); 
+
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
