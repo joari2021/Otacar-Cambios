5c2889ff62b8f48cba58573ac65590def2044cd3
Jorge Luis Ochoa <jorge.uchija2021@gmail.com>
2021-01-21 20:47:05 -0400

    Activacion de Airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -9,7 +9,7 @@
 #
 # Configuration details:
 # https://github.com/airbrake/airbrake-ruby#configuration
-=begin
+
 Airbrake.configure do |c|
   # You must set both project_id & project_key. To find your project_id and
   # project_key navigate to your project's General Settings and copy the values
@@ -72,6 +72,3 @@ Airbrake.configure do |c|
 # https://github.com/airbrake/airbrake#logger
 # Rails.logger = Airbrake::AirbrakeLogger.new(Rails.logger)
 end
-
-
-=end
