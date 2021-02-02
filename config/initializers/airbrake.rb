397522e3d65f2242bea0b94a95cd100203b7311d
Jorge Luis Ochoa <jorge.uchija2021@gmail.com>
2021-02-02 13:38:17 -0400

    ajustes de mensajes de errores del modelo user

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -9,7 +9,7 @@
 #
 # Configuration details:
 # https://github.com/airbrake/airbrake-ruby#configuration
-=begin
+
 Airbrake.configure do |c|
   # You must set both project_id & project_key. To find your project_id and
   # project_key navigate to your project's General Settings and copy the values
@@ -74,4 +74,3 @@ Airbrake.configure do |c|
 
 end
 
-=end
