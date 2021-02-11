2499010fdfda89382cd100943a579291440a268d
Jorge Luis Ochoa <jorge.uchija2021@gmail.com>
2021-02-11 07:54:45 -0400

    modificacion en las columnas de los sub montos de la tabla transactions

--- db/migrate/20210210210821_change_columns_sub_montos_to_transactions.rb
+++ db/migrate/20210210210821_change_columns_sub_montos_to_transactions.rb
@@ -1,7 +1,10 @@
 class ChangeColumnsSubMontosToTransactions < ActiveRecord::Migration[6.0]
   def change
-    change_column :transactions, :sub_monto_a_recibir_1, :decimal, precision: 18, scale: 2
-    change_column :transactions, :sub_monto_a_recibir_2, :decimal, precision: 18, scale: 2
-    change_column :transactions, :sub_monto_a_recibir_3, :decimal, precision: 18, scale: 2
+    remove_column :transactions, :sub_monto_a_recibir_1, :string
+    remove_column :transactions, :sub_monto_a_recibir_2, :string
+    remove_column :transactions, :sub_monto_a_recibir_3, :string
+    add_column :transactions, :sub_monto_a_recibir_1, :decimal, precision: 18, scale: 2
+    add_column :transactions, :sub_monto_a_recibir_2, :decimal, precision: 18, scale: 2
+    add_column :transactions, :sub_monto_a_recibir_3, :decimal, precision: 18, scale: 2
   end
 end
